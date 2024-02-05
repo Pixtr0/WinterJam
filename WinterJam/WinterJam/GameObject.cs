c9dee0d657149b4fc0a884fa33736cd64d2b07ae
Scuttsy <scutts001@gmail.com>
2024-02-05 12:39:02 +0100

    Gamesettings + GameObject

---
+++ WinterJam/WinterJam/GameObject.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using SpriteSheetClass;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinterJam
+{
+    abstract class GameObject
+    {
+        public SpriteSheet Visualisation { get; set; }
+        public bool IsActive { get; set; } = true;
+        public Vector2 TopLeftPosition
+        {
+            get
+            {
+                return Visualisation.TopLeftPosition;
+            }
+            set
+            {
+                Visualisation.TopLeftPosition = value;
+            }
+        }
+        public Vector2 Size
+        {
+            get
+            { return Visualisation.Size; }
+            set { Visualisation.Size = value; }
+        }
+        public virtual void Update(GameTime gameTime)
+        {
+            Visualisation.Update();
+        }
+        public virtual void Draw(SpriteBatch spriteBatch)
+        {
+            if (IsActive)
+            {
+                Visualisation.Draw(spriteBatch);
+            }
+        }
+        public Vector2 CenterGridPosition
+        {
+            get
+            {
+               return GameSettings.grid.GetGridPosition(TopLeftPosition);
+            }
+            set
+            {
+                TopLeftPosition = new Vector2(value.X * GameSettings.Cellsize, value.Y * GameSettings.Cellsize);
+            }
+        }
+        public bool IsAtGridPosition(Vector2 position)
+        {
+            return CenterGridPosition.Equals(position);
+        }
+        public override string ToString()
+        {
+            return $"{CenterGridPosition}";
+        }
+    }
+}
---
+++ WinterJam/WinterJam/GameSettings.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinterJam
+{
+    internal class GameSettings
+    {
+        public static Vector2 WINDOWSIZE { get; set; } = new Vector2(1260, 840);
+        public static int Cellsize { get; set; } = 60;
+        public static int Columns { get; set; } = 11;
+        public static int Rows { get; set; } = 14;
+    }
+}
