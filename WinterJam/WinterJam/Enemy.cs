e3c2cbadb818c558391b170c16eb4ccdaba17989
Pixtr0 <matthias.devilder@gmail.com>
2024-02-05 12:31:08 +0100

    spritesheet + enemy class

---
+++ WinterJam/WinterJam/Enemy.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinterJam
+{
+    internal class Enemy
+    {
+        private Vector2 SpawnPosition {  get; set; }
+        private Vector2 TargetLocation { get; set; }
+
+        private 
+
+        public Enemy(Vector2 Spawn)
+        {
+
+        }
+    }
+}
---
+++ WinterJam/WinterJam/SpriteSheet.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace SpriteSheetClass
+{
+    public class SpriteSheet
+    {
+        public Texture2D Texture { get; set; }
+        public Vector2 TopLeftPosition { get; set; }
+        public Vector2 Size { get; set; }
+        public float Rotation { get; set; }
+        public Color Color { get; set; } = Color.White;
+        public virtual Rectangle DestinationRectangle { get
+            {
+                return new Rectangle(TopLeftPosition.ToPoint(), Size.ToPoint());
+            } 
+        }
+        public virtual Rectangle Hitbox { get
+            {
+                return DestinationRectangle;
+            } 
+        }
+        public Rectangle SourceRectangle { get
+            {
+                int spriteWidth = Texture.Width/ Cols;
+                int spriteHeight = Texture.Height/Rows;
+
+                int spriteCol = CurrentSpriteIndex % Cols;
+                int spriteRow = CurrentSpriteIndex / Cols;
+                return new Rectangle(spriteWidth * spriteCol, spriteHeight * spriteRow, spriteWidth, spriteHeight);
+            }
+        }
+        public int Rows { get; set; }
+        public int Cols { get; set; }
+        public int CurrentSpriteIndex { get; set; } = 0;
+        public bool Loop { get; private set; }
+        public bool IsFinished { get; set; } = false;
+        public SpriteSheet(Texture2D texture, Vector2 topLeftPos, Vector2 size, float rotation, int rows, int cols, int currentSpriteIndex, bool loop)
+        {
+            Texture = texture;
+            TopLeftPosition = topLeftPos;
+            Size = size;
+            Rotation = rotation;
+            Rows = rows;
+            Cols = cols;
+            CurrentSpriteIndex = currentSpriteIndex;
+            Loop = loop;
+         }
+
+        public virtual void Update()
+        {
+            if (!IsFinished)
+            {
+                CurrentSpriteIndex++;
+                if (CurrentSpriteIndex > (Cols * Rows) - 1 && Loop)
+                {
+                    CurrentSpriteIndex = 0;
+                }
+                else if (CurrentSpriteIndex > (Cols * Rows) - 1 && !Loop)
+                {
+                    CurrentSpriteIndex = (Cols * Rows) - 1;
+                    IsFinished = true;
+                }
+            }
+            
+        }
+        public virtual void Draw(SpriteBatch spriteBatch)
+        {
+            Vector2 sourceOrigin = new Vector2(SourceRectangle.Width / 2f, SourceRectangle.Height / 2f);
+
+            Rectangle drMoved = new Rectangle(DestinationRectangle.X + DestinationRectangle.Width / 2, DestinationRectangle.Y + DestinationRectangle.Height / 2, DestinationRectangle.Width, DestinationRectangle.Height);
+
+            spriteBatch.Draw(Texture, drMoved, (Cols > 1 || Rows > 1) ? SourceRectangle : null, Color, Rotation,sourceOrigin, SpriteEffects.None, 0);
+        }
+        public void Play()
+        {
+            CurrentSpriteIndex = 0;
+            IsFinished = false;
+        }
+    }
+}
