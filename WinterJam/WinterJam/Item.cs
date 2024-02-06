bc612bfeaa28574c2d4daf373658eaf348421ccf
Scuttsy <scutts001@gmail.com>
2024-02-06 16:06:21 +0100

    Item bob

--- WinterJam/WinterJam/Item.cs
+++ WinterJam/WinterJam/Item.cs
@@ -1,24 +1,31 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework.Graphics;
+using SpriteSheetClass;
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace WinterJam.Players
+namespace WinterJam
 {
     internal class Item : GameObject
     {
         public int MaxDurability { get; set; }
         public int Durability { get; set; }
-        public Item(int durability)
+
+        public Item(int durability, Texture2D texture, GameObject parent)
         {
             Durability = durability;
             MaxDurability = durability;
+
+
+            Visualisation = new SpriteSheet(texture, Vector2.Zero, parent.Size / 2, 0, 1, 1, 0, false);
         }
         public virtual void Update()
         {
-            Durability--;
+            //Durability--;
 
             if (Durability <= 0)
                 IsActive = false;
